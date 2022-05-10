f081d2acd1f589b50c5097a3d6f010cbd309eed9
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-05-10 17:21:09 -0400

    Update Booklist.js

--- client/src/Components/Booklist.js
+++ client/src/Components/Booklist.js
@@ -4,19 +4,7 @@ import FavoriteList from './FavoriteList'
 
 function Booklist({setFavesArray, b, favesArray, favesList, setFavesList}) {
 
-  // const [faveTitle, setFaveTitle] = useState({})
-  // console.log(b)
-// fave titles set to state, and added to the array from App
-  // const [faveTitle, setFaveTitle] = useState([])
-  // console.log( "faveTitle", faveTitle)
-  // const entryObj = title.entries 
-  // console.log(titles)
-  // const allTitlesInStateArray = titles.map(b=>{ return (<Book key={b.id} b={b}/>)})
-  // const [bookArray] = title.map((b)=>key={)
-  // const bookObj=title.entries
-  // console.log(bookObj)
-  // // const bookArray = bookObj.map(book=>{console.log(book)})
- 
+
   function handleClick(b){
     
    console.log("adding book to faves", b)
@@ -30,16 +18,10 @@ function Booklist({setFavesArray, b, favesArray, favesList, setFavesList}) {
     })
     .then(r=>r.json())
     .then(newTitle=>{
-      // console.log("favetitle", faveTitle)
-      // console.log(newTitle)
       setFavesArray(favesArray=>[ newTitle, ...favesArray])
-      // setFaveTitle(faveTitle)
-      
     })
     
   }
-
-
 
   return (
     <div>
