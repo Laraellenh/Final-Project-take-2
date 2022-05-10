d5d7133d50e2630be83a5415aef3dbace163bee6
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-05-10 17:19:20 -0400

    Update Home.js

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -5,12 +5,10 @@ import BookList from './Booklist'
 // import BookMapper from './BookMapper'
 
 function Home({handleLogOutClick, addFavoriteToList, titles, favesList, setFavesList, setFavesArray, favesArray }) {
-  //  console.log(titles)
-  // once titles.length >0
-      const bookComponentArray =  titles?.map(b=> <BookList favesArray={favesArray} setFavesList={setFavesList} favesList={favesList} setFavesArray={setFavesArray} addFavoriteToList={addFavoriteToList} key={b.id}  b={b}/>)
-   
-  
 
+ const bookComponentArray =  titles?.map(b=> <BookList favesArray={favesArray} setFavesList={setFavesList} favesList={favesList} setFavesArray={setFavesArray} addFavoriteToList={addFavoriteToList} key={b.id}  b={b}/>)
+   
+ 
   return (
     <div>
     <Nav handleLogOutClick={handleLogOutClick} />
@@ -20,9 +18,7 @@ function Home({handleLogOutClick, addFavoriteToList, titles, favesList, setFaves
     className='img'
      src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMy4PkmP0PKx96lB4hiy__M8hH7itaHRIwiQ&usqp=CAU' alt="Vonnegut image"
      />
-    {/* <BookMapper  /> */}
-    {/* <BookList titles={titles} /> */}
-    {/* <FavoriteList   /> */}
+    
     {bookComponentArray}
  
     
