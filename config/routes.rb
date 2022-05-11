704f3bbd623fc61c00cc96ccb8f3ece3fca6dea8
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-05-11 10:40:46 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,14 @@ Rails.application.routes.draw do
   
   resources :users
   resources :favorite_books, only: [:create, :show, :index, :update]
-  # patch '/updated_fave', to: "favorite_books#add_note"
-  # resources :authors
-  # resources :books
+
 
   post "/login", to: "sessions#login"
   get '/me', to: "users#show"
   delete '/logout', to: 'sessions#destroy'
   post "/signup", to: "users#create"
   get '/books', to: "books#get_works"
-  # get '/mylist', to: "favorite_books#show"
-  # get '/books', to: "books#index"
-  # get '/books', to: "books#show"
-  # get "/home", to:
-  # get '/cats', to: "books#cats_cradle"
-  # get '/welcome', to: "books#welcome_to"
+  
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
