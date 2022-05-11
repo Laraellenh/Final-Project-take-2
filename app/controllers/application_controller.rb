4c94e8cf28f7dff879d81fd73fcec9a9fb00c2da
Lara Hocheiser <91838262+Laraellenh@users.noreply.github.com>
2022-05-11 10:35:40 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,8 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   before_action :authorized_user
-  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
-# ?skip before action for fetch books?
-  # def current_user
-  #     User.find_by(id:session[:user_id])
-  # end 
 
   private
 
@@ -14,9 +10,6 @@ class ApplicationController < ActionController::API
       render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end 
   
-  # def not_found(error)
-  #     render json: {error: "#{error.model} Not Found"}, status: :not_found
-  # end 
   def authorized_user
    
     render json: {errors: ["Not authorized"]},status: :unauthorized unless session.include? :user_id
